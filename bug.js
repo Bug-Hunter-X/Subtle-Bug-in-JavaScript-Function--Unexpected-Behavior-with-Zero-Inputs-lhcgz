eccc282c0c2c81de88dd5018ed850398d52246ee
BugHunter-X <askzenai@gmail.com>
2024-12-14 20:20:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,16 @@
+function foo(a, b) {
+  if (a === 0 && b === 0) {
+    return 0; // This line causes an error.
+  } else if (a === 0) {
+    return b;
+  } else if (b === 0) {
+    return a;
+  } else {
+    return a + b;
+  }
+}
+
+console.log(foo(0,0)); //This will return 0
+console.log(foo(1,0)); //This will return 1
+console.log(foo(0,1)); //This will return 1
+console.log(foo(1,1)); //This will return 2
